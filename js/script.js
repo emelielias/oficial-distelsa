ed5d353988b62ee906dc3863cc11420074364a6f
emelielias <emelielias36@gmail.com>
2020-03-07 18:23:04 -0600

    nuevo

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAv9oCek3DkGxuNn3Vna6JuPgpiGk7Gc5U",
+    authDomain: "maxapp-e11de.firebaseapp.com",
+    databaseURL: "https://maxapp-e11de.firebaseio.com",
+    projectId: "maxapp-e11de",
+    storageBucket: "maxapp-e11de.appspot.com",
+    messagingSenderId: "402501589169",
+    appId: "1:402501589169:web:56e24b5c883ba22423aa90",
+    measurementId: "G-E3NT21N5QM"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
